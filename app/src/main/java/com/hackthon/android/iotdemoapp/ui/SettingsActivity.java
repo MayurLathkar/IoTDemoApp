0ce246c770e12746bf854adcf6984710bb3c22e4
Mayur Lathkar <mayurplathkar01@gmail.com>
2016-07-26 13:05:39 +0530

    New image changes

---
+++ app/src/main/java/com/hackthon/android/iotdemoapp/ui/SettingsActivity.java
@@ -0,0 +1,26 @@
+package com.hackthon.android.iotdemoapp.ui;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.hackthon.android.iotdemoapp.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+}
